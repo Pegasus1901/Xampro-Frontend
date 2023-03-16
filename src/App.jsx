afabc3ca2a45aed074691d3cfc23b2f73ba5dfe0
Prathmesh Sanjay Shevale <shevaleprathmesh8@gmail.com>
2023-03-16 16:42:09 +0530

    Table Modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,21 @@
 import React from 'react'
-import { useEffect, useState, useRef } from 'react'
 import axios from 'axios';
+import { useEffect, useState, useRef } from 'react'
 import QuizManager from './components/Test/QuizManager.jsx';
 // import { Box} from '@mui/system';
 import { Container, Box, Typography } from '@mui/material';
 import Login from './components/login/Login.jsx';
 import DDashboard from './components/Dashboard/DDashboard.jsx';
-import Table from './components/Table/Table.jsx';
-import Header from './components/HomePage/Header.jsx';
 import { BrowserRouter } from 'react-router-dom';
-import Footer from './components/HomePage/Footer.jsx';
-import Home from './components/HomePage/Home.jsx';
-// import Profile from './components/ProfilePage/Profile.jsx';
+import Profile from './components/ProfilePage/Profile.jsx';
 import Navigation from './components/VoiceModule/Navigation.jsx';
 import WebcamModule from './components/WebcamModule/WebcamModule.jsx';
-
-
-
+import RenderTable from './components/Table/RenderTable.jsx';
 
 const App = () => {
-  // const [dataTable, setDataTable] = useState([])
-  // console.log(dataTable);
-  // useEffect(() => {
-  //   axios('https://jsonplaceholder.typicode.com/users')
-  //     .then(res => setDataTable(res.data))
-  //     .catch(err => console.log(err))
-  // }, [])
-
-  // const column = [
-  //   { heading: 'Name', value: 'name' },
-  //   { heading: 'Email', value: 'email' },
-  //   { heading: 'Phone', value: 'phone' },
-  //   { heading: 'City', value: 'address.city' },
-  // ]
-
 
   return (
     <>
-
       {/* <BrowserRouter> */}
       {/* <Container maxWidth="sm">
         <Box textAlign="center" mt={5}>
@@ -47,8 +25,7 @@ const App = () => {
       </Container> */}
       {/* </BrowserRouter> */}
 
-      {/* <Table data={dataTable} column={column} /> */}
-
+      {/* <RenderTable/> */}
       {/* <WebcamModule/> */}
       {/* <DDashboard/> */}
       {/* <Table/> */}
---
+++ src/components/Table/RenderTable.jsx
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import React,{useState,useEffect} from 'react'
+import Table from './Table';
+
+
+const RenderTable = () => {
+    const [dataTable, setDataTable] = useState([])
+    console.log(dataTable);
+    useEffect(() => {
+        axios('https://jsonplaceholder.typicode.com/users')
+            .then(res => setDataTable(res.data))
+            .catch(err => console.log(err))
+    }, [])
+
+    const column = [
+        { heading: 'Name', value: 'name' },
+        { heading: 'Email', value: 'email' },
+        { heading: 'Phone', value: 'phone' },
+        { heading: 'City', value: 'address.city' },
+    ]
+  return (
+      <Table data={dataTable} column={column} />
+  )
+}
+
+export default RenderTable
